1c3127cf563d1d38304040ecaa44b09e4c3fcbd2
Ruadhri McDougall <ruadhri.mcdougall@gmail.com>
2023-06-08 10:35:55 +0100

    Created a test file to check that tensor products are working as expected, and pauli matrices operating on a given qubit give the expected result

--- classicalsim.py
+++ classicalsim.py
@@ -10,8 +10,29 @@ import numpy as np
 
 pauli_X = np.array([[0, 1], [1, 0]])
 pauli_Z = np.array([[1, 0], [0, -1]])
+up = np.array([1, 0])
+down = np.array([0, 1])
+
+# maybe an overarching "Operator" class, which the Hamiltonian class inherits from?
 
 class Hamiltonian:
     
-    def __init__(self, qubits):
+    def __init__(self, qubits, form='ising1d'):
         self.__qubits = qubits
+        if form =='ising1d':
+            self.hamiltonian = self.ising_hamiltonian()
+    
+    def ising_hamiltonian(self):
+        pass
+        
+
+class State:
+    
+    def __init__(self):
+        self.__up = np.array([1, 0])
+        self.__down = np.array([0, 1])
+
+def pauli_Z(order):
+    pass
+        
+
